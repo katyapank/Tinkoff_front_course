07d20d0ea80bbf749909a0e9a603f67b0d607033
katyapank <ccoo00@yandex.ru>
2023-12-05 16:19:30 +0400

    the end

--- tack3/src/es6.js
+++ tack3/src/es6.js
@@ -1,27 +1,14 @@
 'use strict';
-// в данных задачах нужно использовать возможности es6
-// ко всем заданиям можно (а местами и нужно) дописать свои тесты в файле es6.spec.js
-// Можно менять параметры функций (например сделать им значения по умолчанию)
-
-// Напишите функцию, которая принимает ФИО пользователя и возвращает
-// строку формата Имя Фамилия
 function fioToName(fio) {
     let [lastName, firstName] = fio.split(' ');
     return `${firstName} ${lastName}`;
 }
 
-// преобразуйте массив чисел так, чтобы в нем остались только
-// уникальные элементы
-// присмотритесь к коллекции "Set"
 function filterUnique(array) {
     const newSet = new Set(array);
     return Array.from(newSet);
 }
 
-// Задача: разница зарплат
-// в функцию приходит массив из n зарплат сотрудников фирмы
-// ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
-// сотрудника превышает зарплату самого низкооплачиваемого
 function calculateSalaryDifference(array) {
     if (!array.length) return false;
     else {
@@ -37,11 +24,6 @@ function calculateSalaryDifference(array) {
     }
 }
 
-// Реализуйте класс "словарь слов" (как толковый словарь)
-// класс должен быть безопасным и работать только со словами
-// присмотритесь к коллекции "Map"
-// Словарь - (string, string), и все это не null и не undefined
-// * покройте класс тестами
 class Dictionary {
     constructor() {
         this.map = new Map();
@@ -60,15 +42,15 @@ class Dictionary {
         if (typeof def === 'string') {
             if (this.map.has(def)) console.log(def + ' ' + this.get(def));
             else return false;
-        } else return false; //неверный тип
+        } else return false;
     }
     remove(def) {
         if (typeof def === 'string') {
             if (this.map.has(def)) {
                 this.map.delete(def);
                 return true;
-            } else return false; //нет в словаре
-        } else return false; //неверный тип
+            } else return false;
+        } else return false;
     }
 
     showAllWords() {
